e143e9f00ae111e7991d9265a6f4ad6314772ef0
Danilo Argolo <dan_argolo@hotmail.com>
2023-03-23 17:30:44 -0300

    [FEAT] Implementa Services

---
+++ src/services/products.js
@@ -0,0 +1,26 @@
+const { productsModel } = require('../models');
+
+const getAll = async () => {
+  try {
+    const products = await productsModel.getAll();
+    return { status: 200, message: products };
+  } catch (error) {
+    console.log(error);
+  }
+}
+const getById = async (id) => {
+  try {
+    const product = await productsModel.getById(id);
+    if (!product) {
+      return {
+        error: 404, message: 'Product not found' } }
+    return {  message: product };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  getAll,
+  getById
+}
