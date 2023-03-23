da68211ec82d5b75b609f67a955eadd45548b567
Danilo Argolo <dan_argolo@hotmail.com>
2023-03-23 17:29:56 -0300

    [FEAT] Implementa Models

---
+++ src/models/products.js
@@ -0,0 +1,19 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+  const QUERY = 'SELECT * FROM StoreManager.products';
+  const [response] = await connection.execute(QUERY);
+
+  return response;
+}
+const getById = async (id) => {
+  const QUERY = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const [[response]] = await connection.execute(QUERY, [id]);
+
+  return response;
+}
+
+module.exports = {
+  getAll,
+  getById,
+}
