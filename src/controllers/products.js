a4225bb40d0be213bd6bb20dbad72108a88b1533
Danilo Argolo <dan_argolo@hotmail.com>
2023-03-24 11:46:00 -0300

    [REFACT] Remove service/ try-catch e add obj type.

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,15 +1,15 @@
 const { productsService } = require('../services');
 
 const getAll = async (_req, res) => {
-  const { status, message } = await productsService.getAll();
+  const { message } = await productsService.getAll();
 
-  return res.status(status).json(message);
+  return res.status(200).json(message);
 };
 const getById = async (req, res) => {
   const { id } = req.params;
-  const { error, message } = await productsService.getById(id);
+  const { type, message } = await productsService.getById(id);
   
-  if (error) { return res.status(error).json({ message }); }
+  if (type) { return res.status(404).json({ message }); }
 
   return res.status(200).json(message);
 };
--- src/services/products.js
+++ src/services/products.js
@@ -1,24 +1,17 @@
 const { productsModel } = require('../models');
 
 const getAll = async () => {
-  try {
-    const products = await productsModel.getAll();
-    return { status: 200, message: products };
-  } catch (error) {
-    console.log(error);
-  }
+  const products = await productsModel.getAll();
+  return { message: products };
+
 };
 const getById = async (id) => {
-  try {
-    const [[product]] = await productsModel.getById(id);
-    if (!product) {
-      return {
-        error: 404, message: 'Product not found' }; 
+  const [[product]] = await productsModel.getById(id);
+  if (!product) {
+    return {
+      type: 'PRODUCT NOT FOUND', message: 'Product not found' }; 
 }
-    return { message: product };
-  } catch (error) {
-    console.log(error);
-  }
+    return { type: null, message: product };
 };
 
 module.exports = {
