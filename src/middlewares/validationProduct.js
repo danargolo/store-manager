cd57bac6878394c4b19e41dc247253cd0ad45da8
Danilo Argolo <dan_argolo@hotmail.com>
2023-03-25 01:25:34 -0300

    [Feat] Req 04 - Implementa Joi schemas

--- src/middlewares/validationProduct.js
+++ src/middlewares/validationProduct.js
@@ -1,9 +1,17 @@
-const validationProduct = (req, _res, next) => {
+const { productSchema } = require('../schemas/productSchema');
+
+const validationProduct = (req, res, next) => {
   const { name } = req.body;
+  const { error } = productSchema.validate(name);
 
   if (!name) {
     return res.status(400).json({ message: '"name" is required' });
   }
+
+  if (error) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+
   return next();
 };
 
